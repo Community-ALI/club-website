fe3016440b761131a4329b8e5bf3dfac6c24c21a
OBenjOne <bschoolland@gmail.com>
2023-12-22 15:48:58 -0800

    test(database-read-write)
    Added two files that are capable of reading and writing to the database, to be used as a test

---
+++ app/api/test-read-write/route.js
@@ -0,0 +1,42 @@
+import { sql } from "@vercel/postgres";
+// Get returns all users
+export async function GET() {
+    try {
+        const data = await sql `SELECT * FROM USERS`;
+        // remove the password from the response for obvious reasons
+        return new Response(JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return new Response(JSON.stringify({ message: "Error" }), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+}
+
+// Post adds a user to the database
+export async function POST(request) {
+    try {
+        const body = await request.json();
+        console.log(body);
+        await sql `INSERT INTO USERS (Email, Password, Club_name) VALUES (${body.email}, ${body.password}, ${body.club_name})`;
+        const data = { message: "Success" };
+        return new Response(JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return new Response(JSON.stringify({ message: "Error" }), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+}
