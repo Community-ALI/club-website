55b047ae812d758be3df7d717f2b166ea8768d4a
OBenjOne <bschoolland@gmail.com>
2024-01-04 13:27:02 -0800

    feat(page.js)
    Add a hash to the url

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-"use client";
-import React, { useState } from "react";
+'use client';
+import React, { useState, useEffect } from "react";
 import Navbar from "./reusable-components/navbar";
 import Home from "./landing-tabs/home";
 import Title from "./reusable-components/title";
@@ -13,18 +13,42 @@ import ForgotPassword from "./landing-tabs/forgot-password";
 export default function LandingPage() {
   const [currentPage, setCurrentPage] = useState("home");
 
+  // Function to update the current page and the URL hash
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+    window.location.hash = newPage;
+  };
+
+  // Effect to handle URL hash on page load
+  useEffect(() => {
+    const hash = window.location.hash.replace("#", "");
+    if (hash) setCurrentPage(hash);
+  }, []);
+
+  // Effect to handle URL hash change
+  useEffect(() => {
+    const handleHashChange = () => {
+      const newHash = window.location.hash.replace("#", "");
+      setCurrentPage(newHash);
+    };
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => window.removeEventListener("hashchange", handleHashChange);
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen bg-mjc bg-cover bg-no-repeat 
     bg-center w-full h-full z-[-1]">
-      <Navbar setCurrentPage={setCurrentPage} />
+      <Navbar setCurrentPage={handlePageChange} />
       <Title></Title>
       <main className="flex-grow">
-        {currentPage === "home" && <Home/>}
-        {currentPage === "signIn" && <SignIn setCurrentPage={setCurrentPage}/>}
-        {currentPage === "createAccount" && <CreateAccount setCurrentPage={setCurrentPage}/>}
-        {currentPage === "contact" && <ContactInfo/>}
-        {currentPage === "techSupport" && <TechSupport></TechSupport>}
-        {currentPage === "forgotPassword" && <ForgotPassword setCurrentPage={setCurrentPage}/>}
+        {currentPage === "home" && <Home />}
+        {currentPage === "signIn" && <SignIn setCurrentPage={handlePageChange} />}
+        {currentPage === "createAccount" && <CreateAccount setCurrentPage={handlePageChange} />}
+        {currentPage === "contact" && <ContactInfo />}
+        {currentPage === "techSupport" && <TechSupport />}
+        {currentPage === "forgotPassword" && <ForgotPassword setCurrentPage={handlePageChange} />}
       </main>
       <Deadline></Deadline>
     </div>
