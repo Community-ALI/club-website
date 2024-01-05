d8efcba588029477bd48dd74d31f3ed654b3d506
MasonPartridge <masonepartridge@gmail.com>
2024-01-05 12:18:09 -0800

    Feat(ClubOfficersSection): Add club officers text fields

--- app/club-application/ClubOfficersSection.jsx
+++ app/club-application/ClubOfficersSection.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ClubApplicationHeaderSection from "./ClubApplicationHeaderSection";
+import ClubApplicationTextField from "./ClubApplicationTextField";
 
 export default function ClubOfficersSection() {
   const [clubOfficers, setClubOfficers] = useState([
@@ -23,6 +24,49 @@ export default function ClubOfficersSection() {
         </p>
         <hr />
       </div>
+    </div>
+  );
+}
+
+function OfficerField(props) {
+  const { officerIndex, setClubOfficers, clubOfficers } = props;
+  function setOfficerField(field, value) {
+    setClubOfficers(
+      clubOfficers.map((officer, index) =>
+        index === officerIndex ? { ...officer, [field]: value } : officer
+      )
+    );
+  }
+  return (
+    <div className="">
+      <h2 className="mt-8">{clubOfficers[officerIndex].role}</h2>
+
+      <ClubApplicationTextField
+        label="Full Name"
+        value={clubOfficers[officerIndex].name}
+        onChange={(e) => setOfficerField("name", e.target.value)}
+      />
+
+      <ClubApplicationTextField
+        label="School Email"
+        value={clubOfficers[officerIndex].email}
+        onChange={(e) => setOfficerField("email", e.target.value)}
+        
+      />
+
+      <ClubApplicationTextField
+        label="Phone Number"
+        value={clubOfficers[officerIndex].phoneNumber}
+        onChange={(e) => setOfficerField("phoneNumber", e.target.value)}
+      />
+
+      <ClubApplicationTextField
+        label="Major"
+        value={clubOfficers[officerIndex].major}
+        onChange={(e) => setOfficerField("major", e.target.value)}
+      />
+
+      
     </div>
   );
 }
