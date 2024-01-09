734211f26f70fdd2327f1a24179d1bdd325fbbfb
OBenjOne <bschoolland@gmail.com>
2024-01-09 12:16:37 -0800

    feat(page.jsx): remove test button

--- app/club-application/page.jsx
+++ app/club-application/page.jsx
@@ -345,8 +345,7 @@ export default function ClubAgreementPage() {
       id="club-application-page"
       className="flex justify-center py-[220px] min-h-screen h-full bg-gradient-to-bl to-[#112B66] from-[#508BB8] gap-[58px]"
     >
-      {/* a test button to submit the form */}
-      <button onClick={submitDraft}>Submit Draft</button>
+
       <div className="bg-offWhite w-[900px] h-fit">
         {sections[currentSection].form}
       </div>
