ae06d69b9bbd067ca24f9f31cbbfda336fdc6fd3
MasonPartridge <masonepartridge@gmail.com>
2024-01-16 15:12:13 -0800

    Feat(submit): Add email backend pdf api
    I don't know if this is exactly what been wanted by ehh~

---
+++ app/api/submit/email/route.js
@@ -0,0 +1,21 @@
+import sendEmail from "../../../utils.js/email.js";
+
+export async function POST(request) {
+  const body = await request.json();
+  try {
+    sendEmail(
+      body.email,
+      `${body.clubTitle} Application PDF`,
+      `honestly idk what to put here. but here's the link to the pdf: ${body.pdfLink}`
+    );
+  } catch (err) {
+    console.log(err);
+    const data = { message: "Internal server error" };
+    return new Response(JSON.stringify(data), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 500,
+    });
+  }
+}
