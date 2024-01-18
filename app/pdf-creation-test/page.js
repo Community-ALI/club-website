5d4366eff59b00b2951f3560fd88050f2cf0af30
Adrean Cajigas <adrean.cajigas@gmail.com>
2024-01-17 17:54:16 -0800

    feat: pdf spacing and styling

--- app/pdf-creation-test/page.js
+++ app/pdf-creation-test/page.js
@@ -1,122 +1,165 @@
-'use client';
-import PdfComponent from './pdfComponent';
+"use client";
+import PdfComponent from "./pdfComponent";
 
 export default function PDFCreationTestPage() {
-    
-    function getUniqueSelector(element) {
-        if (element.id) {
-            return `#${element.id}`;
-        } else if (element.className) {
-            return `.${element.className.split(' ').join('.')}`;
-        } else {
-            return element.tagName.toLowerCase();
-        }
+  function getUniqueSelector(element) {
+    if (element.id) {
+      return `#${element.id}`;
+    } else if (element.className) {
+      return `.${element.className.split(" ").join(".")}`;
+    } else {
+      return element.tagName.toLowerCase();
+    }
+  }
+
+  function extractStyles(element) {
+    var styles = "";
+    var computedStyles = window.getComputedStyle(element);
+
+    // Generate the selector
+    var selector = getUniqueSelector(element);
+
+    // Start the CSS rule
+    styles += `${selector} {`;
+
+    // Add the computed styles
+    for (let prop of computedStyles) {
+      styles += `${prop}: ${computedStyles.getPropertyValue(prop)}; `;
     }
 
-    function extractStyles(element) {
-        var styles = '';
-        var computedStyles = window.getComputedStyle(element);
-    
-        // Generate the selector
-        var selector = getUniqueSelector(element);
-    
-        // Start the CSS rule
-        styles += `${selector} {`;
-    
-        // Add the computed styles
-        for (let prop of computedStyles) {
-            styles += `${prop}: ${computedStyles.getPropertyValue(prop)}; `;
-        }
-    
-        styles += '} ';
-    
-        // Process children
-        Array.from(element.children).forEach(child => {
-            styles += extractStyles(child);
-        });
-    
-        return styles;
-    }
+    styles += "} ";
 
-    function giveIdToAllElements(element) {
-        element.id = Math.random().toString(36).substring(7);
-        Array.from(element.children).forEach(child => {
-            giveIdToAllElements(child);
-        });
-    }
+    // Process children
+    Array.from(element.children).forEach((child) => {
+      styles += extractStyles(child);
+    });
 
-    function copyElementWithStyles(elementId) {
-        var element = document.getElementById(elementId);
-        // give each item in the pdf component a unique id
-        giveIdToAllElements(element);
-        // change the id of the pdf component to pdf
-        element.id = 'pdf';
-        // Extract HTML
-        var htmlContent = element.outerHTML;
-    
-        // Extract CSS
-        var cssContent = extractStyles(element);
-        
-        // Return HTML and CSS
-        return { html: htmlContent, css: cssContent };
-    }
+    return styles;
+  }
 
-    function generatePDF() {
-        // copy the html and css of the pdf component
-        const { html, css } = copyElementWithStyles('pdf');
-        // this is the html and css of the pdf component
-        const data = `
+  function giveIdToAllElements(element) {
+    element.id = Math.random().toString(36).substring(7);
+    Array.from(element.children).forEach((child) => {
+      giveIdToAllElements(child);
+    });
+  }
+
+  function copyElementWithStyles(elementId) {
+    var element = document.getElementById(elementId);
+    // give each item in the pdf component a unique id
+    giveIdToAllElements(element);
+    // change the id of the pdf component to pdf
+    element.id = "pdf";
+    // Extract HTML
+    var htmlContent = element.outerHTML;
+
+    // Extract CSS
+    var cssContent = extractStyles(element);
+
+    // Return HTML and CSS
+    return { html: htmlContent, css: cssContent };
+  }
+
+  function generatePDF() {
+    // copy the html and css of the pdf component
+    const { html, css } = copyElementWithStyles("pdf");
+    // this is the html and css of the pdf component
+    const data = `
         <!DOCTYPE html>
-        <html>
         <head>
-            <style>
-            h1 {
-                font-family: 'Heavitas', sans-serif;
-                color: aqua;
-                background-color: brown;
-            }
-            
-            p {
-                color: red;
-            }
-            
-            .officer {
-                page-break-inside: avoid;
-                color: blue;
-            }
-            </style>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
         </head>
         <body>
+        
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
+
+        *{
+            font-family: "Roboto";
+            color: black;
+            margin: 0px;
+            padding: 0px;
+        }
+
+        .pdf {
+            padding: 20px 60px;
+        }
+
+        h1 {
+            text-align: center;
+            margin-top: 30px;
+            margin-bottom: 20px;
+        }
+
+        .container {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            gap: 10px;
+            margin-bottom: 20px;
+        }
+
+        .section-title {
+            font-size: 20px;
+            margin-bottom: 20px;
+        }
+
+        .header {
+            font-size: 15px;
+            white-space: nowrap;
+        }
+
+        .data {
+            font-size: 14px;
+        }
+
+        .info {
+            margin-bottom: 50px;
+        }
+        
+        .advisor {
+            margin-bottom: 50px;
+        }
+
+        .officer {
+            margin-bottom: 50px;
+            page-break-inside: avoid;
+        }
+        </style>
+
             ${html}
         </body>
         </html>
-        `
-        // send the html to the api
-        fetch('/api/pdf', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // send the html of the pdf component
-            body: JSON.stringify({ data: data }),
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            // Download or display the PDF
-            const pdfUrl = URL.createObjectURL(blob);
-            window.open(pdfUrl);
-        });
-    }
-    return (
-        <>
-            <div className="text-darkBlue">
-                <button onClick={generatePDF}>Generate PDF</button>
-                <div>
-                    <div id="pdf">
-                        <PdfComponent title="Test Title" text="Test Text" />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+        `;
+    // send the html to the api
+    fetch("/api/pdf", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // send the html of the pdf component
+      body: JSON.stringify({ data: data }),
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Download or display the PDF
+        const pdfUrl = URL.createObjectURL(blob);
+        window.open(pdfUrl);
+      });
+  }
+  return (
+    <>
+      <div className="text-darkBlue">
+        <button onClick={generatePDF}>Generate PDF</button>
+        <div>
+          <div id="pdf">
+            <PdfComponent title="Test Title" text="Test Text" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- app/pdf-creation-test/pdfComponent.js
+++ app/pdf-creation-test/pdfComponent.js
@@ -1,124 +1,185 @@
 function MyComponent() {
   // the JSON object that will be used to populate the PDF
   const form = {
-    "clubInformation": {
-        "clubName": "test",
-        "meetingDaysAndTime": "test2",
-        "meetingLocation": "Both",
-        "buildingAndRoomNumber": "adgadsgasdg",
-        "zoomLink": "adsgasdgasdg"
+    clubInformation: {
+      clubName: "MJC Business Club",
+      meetingDaysAndTime: "Every Tuesday and Friday at 9am",
+      meetingLocation: "Both",
+      buildingAndRoomNumber: "Center of Advanced Tech Room 213",
+      zoomLink:
+        "https://us02web.zoom.us/j/86540662672?pwd=TWYxL3VENlM2TmhiYndiKy9QaHZndz09",
     },
-    "clubAdvisors": [
-        {
-            "name": "test",
-            "email": "adgasd",
-            "phoneNumber": "aaa",
-            "title": "aaa"
-        }
+    clubAdvisors: [
+      {
+        name: "Leslie Vaughn",
+        email: "LeslieVaughn@yosemite.edu",
+        phoneNumber: "(209)-505-3894",
+        title: "Business Professor",
+      },
     ],
-    "clubOfficers": [
-        {
-            "role": "Club President",
-            "name": "test",
-            "email": "asdgadsg",
-            "wNumber": "dsgadasg",
-            "phoneNumber": "adsgdsag",
-            "major": "adsgdsga",
-            "gradeLevel": "Freshman (1st Year)"
-        },
-        {
-            "role": "ICC Representative",
-            "name": "asdgasdg",
-            "email": "sadgdsa",
-            "wNumber": "dsaggsda",
-            "phoneNumber": "adsgsdag",
-            "major": "asdgdsag",
-            "gradeLevel": "Senior (4th Year)"
-        },
-        {
-            "role": "Club Vice President",
-            "placeholder": "This club does not have a Club Vice President"
-        },
-        {
-            "role": "Club Secretary",
-            "name": "asdgsdgag",
-            "email": "asdgsdag",
-            "wNumber": "asdgsdag",
-            "phoneNumber": "dsagasdg",
-            "major": "dgsadsg",
-            "gradeLevel": "5+ Years"
-        },
-        {
-            "role": "Club Treasurer",
-            "placeholder": "This club does not have a Club Treasurer"
-        },
-        {
-            "role": "Club Social Media Manager",
-            "name": "asdfasd",
-            "email": "asdsad",
-            "wNumber": "dsadsa",
-            "phoneNumber": "fgff",
-            "major": "ffffffffff",
-            "gradeLevel": "Junior (3rd Year)"
-        }
+    clubOfficers: [
+      {
+        role: "Club President",
+        name: "Adrean Cajigas",
+        email: "adrean964837@my.yosemite.edu",
+        wNumber: "964837",
+        phoneNumber: "(209)-505-3894",
+        major: "Computer Science",
+        gradeLevel: "Freshman (1st Year)",
+      },
+      {
+        role: "ICC Representative",
+        name: "Benjamin Schoolland",
+        email: "benjamin849082@my.yosemite.edu",
+        wNumber: "894249",
+        phoneNumber: "(209)-690-69420",
+        major: "Computer Science",
+        gradeLevel: "Senior (4th Year)",
+      },
+      {
+        role: "Club Vice President",
+        placeholder: "This club does not have a Club Vice President",
+      },
+      {
+        role: "Club Secretary",
+        name: "Kirill Kovalenko",
+        email: "kirill123456@my.yosemite.edu",
+        wNumber: "123456",
+        phoneNumber: "(209)-231-5431",
+        major: "Business Administration",
+        gradeLevel: "5+ Years",
+      },
+      {
+        role: "Club Treasurer",
+        placeholder: "This club does not have a Club Treasurer",
+      },
+      {
+        role: "Club Social Media Manager",
+        placeholder: "This club does not have a Club Treasurer",
+      },
     ],
-    "clubMembers": [
-        {
-            "name": "adfasdg",
-            "email": "adsgsadgsa",
-            "wNumber": "0917714"
-        },
-        {
-            "name": "adsg",
-            "email": "adsfg",
-            "wNumber": "asdgfhgjm,.k"
-        },
-        {
-            "name": "adfsfdas",
-            "email": "fdsadfas",
-            "wNumber": "jhgfds"
-        },
-        {
-            "name": "dsafgfdhh",
-            "email": "fasddfsadsaf",
-            "wNumber": "hgfds"
-        },
-        {
-            "name": "ghdjfkglh;",
-            "email": "FDGZHxjkfl",
-            "wNumber": " vgt"
-        }
+    clubMembers: [
+      {
+        name: "adfasdg",
+        email: "adsgsadgsa",
+        wNumber: "0917714",
+      },
+      {
+        name: "adsg",
+        email: "adsfg",
+        wNumber: "asdgfhgjm,.k",
+      },
+      {
+        name: "adfsfdas",
+        email: "fdsadfas",
+        wNumber: "jhgfds",
+      },
+      {
+        name: "dsafgfdhh",
+        email: "fasddfsadsaf",
+        wNumber: "hgfds",
+      },
+      {
+        name: "ghdjfkglh;",
+        email: "FDGZHxjkfl",
+        wNumber: " vgt",
+      },
     ],
-    "clubAgreement": [
-        {
-            "role": "Club President",
-            "signature": "test",
-            "date": "123"
-        },
-        {
-            "role": "Club Advisor",
-            "signature": "testtaketwo",
-            "date": "234"
-        }
-    ]
-}
+    clubAgreement: [
+      {
+        role: "Club President",
+        signature: "test",
+        date: "123",
+      },
+      {
+        role: "Club Advisor",
+        signature: "testtaketwo",
+        date: "234",
+      },
+    ],
+  };
   return (
-    <div>
-      <h1>
-        {form.clubInformation.clubName}
-      </h1>
-      <p>{form.clubInformation.meetingDaysAndTime}</p>
+    <div class="pdf">
+      <h1>{form.clubInformation.clubName}</h1>
+
+      <div class="info">
+        <h2 class="section-title">Club Information</h2>
+
+        <div class="container">
+          <h2 class="header">Meeting Days and Time:</h2>
+          <p class="data">{form.clubInformation.meetingDaysAndTime}</p>
+        </div>
+
+        <div class="container">
+          <h2 class="header">Meeting Location:</h2>
+          <p class="data">{form.clubInformation.buildingAndRoomNumber}</p>
+        </div>
+
+        <div class="container">
+          <h2 class="header">Zoom Link:</h2>
+          <p class="data" id="link">
+            {form.clubInformation.zoomLink}
+          </p>
+        </div>
+      </div>
+
+      {form.clubAdvisors.map((advisor) => (
+        <div class="advisor">
+          <h2 class="section-title">Club Advisor</h2>
+
+          <div class="container">
+            <h2 class="header">Name:</h2>
+            <p class="data">{advisor.name}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">Email:</h2>
+            <p class="data">{advisor.email}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">Phone Number:</h2>
+            <p class="data">{advisor.phoneNumber}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">Faculty Title:</h2>
+            <p class="data">{advisor.title}</p>
+          </div>
+        </div>
+      ))}
 
       {/* map all the officers */}
       {form.clubOfficers.map((officer) => (
-        <div class = 'officer'>
-          <h2>{officer.role}</h2>
-          <p>{officer.name}</p>
-          <p>{officer.email}</p>
-          <p>{officer.wNumber}</p>
-          <p>{officer.phoneNumber}</p>
-          <p>{officer.major}</p>
-          <p>{officer.gradeLevel}</p>
+        <div class="officer">
+          <h2 class="section-title">{officer.role}</h2>
+
+          <div class="container">
+            <h2 class="header">Name:</h2>
+            <p class="data">{officer.name}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">Email:</h2>
+            <p class="data">{officer.email}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">W Number:</h2>
+            <p class="data">{officer.wNumber}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">Major:</h2>
+            <p class="data">{officer.major}</p>
+          </div>
+
+          <div class="container">
+            <h2 class="header">Grade Level:</h2>
+            <p class="data">{officer.gradeLevel}</p>
+          </div>
+
+          <p class="data">{officer.placeholder}</p>
         </div>
       ))}
 
